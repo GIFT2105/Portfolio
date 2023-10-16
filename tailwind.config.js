251dbdf783fc7aa55f0991bd80f3b0f7d80d5ef6
GIFT2105 <letuka.gift21@gmail.com>
2023-10-16 20:56:29 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:'class',
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+   
+  ],
+  theme: {
+    extend: {
+    fontFamily: 
+    {
+      burtons:"burtons",
+      new: ["Playfair Display", "serif"]
+    },
+
+    },
+  },
+  plugins: [],
+}
